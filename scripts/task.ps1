ba5daa281b3517995032b42202ce866b002d46de
Anastasiia <anastasiia.chikrizova@dashdevs.com>
2026-07-30 14:17:04 +0300

    fix ai comments

--- scripts/task.ps1
+++ scripts/task.ps1
@@ -3,12 +3,13 @@ $files = Get-ChildItem ../data
 $arr = @()
 
 foreach ($file in $files) {
-    $decodedObject = Get-Content $file | ConvertFrom-Json
-    if ($decodedObject.Name -eq "Standard_B2pts_v2") {
-        $regionName =  "$file".Replace('.json', '').Replace('/Users/anastasiiachikrizova/Documents/azure_task_7_work_with_objects_in_powershell/data/','')
-        $arr += $regionName
+    $decodedObject = Get-Content $file.FullName | ConvertFrom-Json
+    foreach ($vmSize in $decodedObject) {
+        if ($vmSize.Name -eq "Standard_B2pts_v2") {
+            $regionName = $file.Name.Replace('.json', '')
+            $arr += $regionName
+        }
     }
 }
 
-
-$arr | ConvertTo-Json | Out-File -Path./result.json
+$arr | ConvertTo-Json | Out-File -Path ./result.json
