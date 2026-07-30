3e0305f61f75229b34d939c0fb0fdde7c1b8c71c
Anastasiia <anastasiia.chikrizova@dashdevs.com>
2026-07-30 14:22:15 +0300

    fix ai comments

--- task.ps1
+++ task.ps1
@@ -1,1 +1,15 @@
-# Write your code here
+$files = Get-ChildItem ./data
+
+$arr = @()
+
+foreach ($file in $files) {
+    $decodedObject = Get-Content $file.FullName | ConvertFrom-Json
+    foreach ($vmSize in $decodedObject) {
+        if ($vmSize.Name -eq "Standard_B2pts_v2") {
+            $regionName = $file.Name.Replace('.json', '')
+            $arr += $regionName
+        }
+    }
+}
+
+$arr | ConvertTo-Json | Out-File -Path ./result.json
