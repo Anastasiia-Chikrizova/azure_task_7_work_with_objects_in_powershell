a027de77f9f7cd1abdec9e29bf6180c72bdb00a8
Anastasiia <anastasiia.chikrizova@dashdevs.com>
2026-07-30 14:10:05 +0300

    Add PowerShell script to process JSON files and extract specific region names

---
+++ scripts/task.ps1
@@ -0,0 +1,15 @@
+$files = Get-ChildItem ../data
+
+$arr = @()
+
+foreach ($file in $files) {
+    $decodedObject = Get-Content $file | ConvertFrom-Json
+    if ($decodedObject.Name -eq "Standard_B2pts_v2") {
+        $regionName =  "$file".Replace('.json', '').Replace('/Users/anastasiiachikrizova/Documents/azure_task_7_work_with_objects_in_powershell/data/','')
+        $arr += $regionName
+    }
+}
+
+
+$arr | ConvertTo-Json | Out-File -Path./result.json
+#Get-Content ./result.json
